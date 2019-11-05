28f6527c10cb5320ed6861217f86874d335f675a
pengchujin <2093959501@qq.com>
2019-11-05 16:45:28 +0800

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module subsrcribe
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/jinzhu/gorm v1.9.11
+	github.com/satori/go.uuid v1.2.0
+)
---
+++ models/user.go
@@ -0,0 +1,28 @@
+package models 
+
+import (
+	"time"
+	"github.com/satori/go.uuid"
+	"github.com/jinzhu/gorm"
+)
+
+type User struct {
+	ID int `gorm:"AUTO_INCREMENT"`
+	UUID  uuid.UUID   `json:"id"`
+	Email string
+	UserName string
+	EncriptdPassword string
+	CreatedAt time.Time  `json:"created_at"`
+  UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (user *User) BeforeCreate(scope *gorm.Scope) error {
+  scope.SetColumn("CreatedAt", time.Now())
+  scope.SetColumn("ID", uuid.NewV4().String())
+  return nil
+}
+
+func (user *User) BeforeUpdate(scope *gorm.Scope) error {
+  scope.SetColumn("UpdatedAt", time.Now())
+  return nil
+}
